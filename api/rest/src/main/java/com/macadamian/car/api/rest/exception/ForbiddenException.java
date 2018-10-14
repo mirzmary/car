62ad970488bec6d106756bb3cae7926dff17bafa
mary.mirzoyan <mary.mirzoyan@gmail.com>
2018-10-15 02:32:11 +0400

    minor changes

--- api/rest/src/main/java/com/macadamian/car/api/rest/exception/ForbiddenException.java
+++ api/rest/src/main/java/com/macadamian/car/api/rest/exception/ForbiddenException.java
@@ -5,7 +5,8 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value= HttpStatus.FORBIDDEN, reason="Permission Denied", code = HttpStatus.FORBIDDEN)
 public class AccessToResourceRestrictedException extends  LoggerAwareApiRuntimeException {
-    private static final long serialVersionUID = -8823095429513116891L;
+
+    private static final long serialVersionUID = -7918779496573123237L;
 
     public AccessToResourceRestrictedException(final Object caller, final String message, final Object... args){
         super(caller, message, args);
